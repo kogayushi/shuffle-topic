53965c6a713471dd04d0bca10400f897bc696ea9
yushi.koga <youshe.old.pleasure@gmail.com>
2020-05-09 16:56:37 +0900

    implement timer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
   <div id="app">
     <div>
-      <h2>お題</h2>
-      <transition name="fade">
-        <p>{{ currentTopic }}</p>
-      </transition>
+      <h2>あなたのお題</h2>
+      <p>{{ currentTopic }}</p>
     </div>
     <div>
-      <button @click="start">スタート</button>
+      <button :disabled="rouletteId" @click="start">スタート</button>
       <button @click="stop">ストップ</button>
+    </div>
+    <div>
+      <h2>タイマー</h2>
+      <p>残り{{ currentRemainingSecond }} 秒</p>
+    </div>
+    <div>
+      <h2>お題</h2>
+      <p>
+        <input v-model="topics[1]" />
+      </p>
+      <p>
+        <input v-model="topics[2]" />
+      </p>
+      <p>
+        <input v-model="topics[3]" />
+      </p>
+      <p>
+        <input v-model="topics[4]" />
+      </p>
+      <p>
+        <input v-model="topics[5]" />
+      </p>
+      <p>
+        <input v-model="topics[6]" />
+      </p>
+    </div>
+    <div>
+      <h2>タイマー設定（秒）</h2>
+      <p>
+        <input v-model="remainingSecond" />
+      </p>
     </div>
   </div>
 </template>
@@ -28,28 +57,49 @@ export default {
         3: "グロービスに入社して〇〇だったこと",
         4: "受講あるある",
         5: "好きな芸能人",
-        6: "尊敬する上司"
+        6: "TBD"
       },
-      intervalId: null
+      rouletteId: null,
+      timeId: null,
+      remainingSecond: 360,
+      currentRemainingSecond: 360
     };
   },
   methods: {
     start() {
       console.log("start");
-      if (!this.intervalId) {
+      if (!this.rouletteId) {
         const id = setInterval(() => {
           const index = Math.floor(Math.random() * (max + 1 - min)) + min;
           this.currentTopic = this.topics[index];
         }, 200);
-        this.intervalId = id;
+        this.rouletteId = id;
+      }
+
+      if (this.timeId) {
+        clearInterval(this.timeId);
+        this.timeId = null;
       }
     },
     stop() {
       console.log("stop");
-      if (this.intervalId) {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
+      if (this.rouletteId) {
+        clearInterval(this.rouletteId);
+        this.rouletteId = null;
       }
+
+      this.currentRemainingSecond = this.remainingSecond;
+      if (this.timeId) {
+        clearInterval(this.timeId);
+      }
+      const id = setInterval(() => {
+        if (this.currentRemainingSecond > 0) {
+          this.currentRemainingSecond -= 1;
+        } else {
+          clearInterval(this.timeId);
+        }
+      }, 1000);
+      this.timeId = id;
     }
   }
 };
@@ -63,15 +113,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  will-change: opacity;
-  transition: opacity 225ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
 }
 </style>
